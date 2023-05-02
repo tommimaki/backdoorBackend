249b4c3a2336c08a48c8060e2f30a19e38c2b04f
tommibish <74586216+tommimaki@users.noreply.github.com>
2023-05-03 02:06:10 +0300

    route addition for inconstructionsingleID fetch

--- controllers/siteController.js
+++ controllers/siteController.js
@@ -9,3 +9,16 @@ exports.getAllSites = async (req, res) => {
     res.status(500).json({ error: "Failed to fetch sites" });
   }
 };
+
+exports.getSiteById = async (req, res) => {
+  try {
+    const site = await Site.findById(req.params.id);
+    if (!site) {
+      res.status(404).json({ error: "Site not found" });
+    } else {
+      res.json(site);
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch site" });
+  }
+};
--- routes/siteRoutes.js
+++ routes/siteRoutes.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const siteController = require("../controllers/siteController");
 
 router.get("/sites", siteController.getAllSites);
+router.get("/sites/:id", siteController.getSiteById);
 
 module.exports = router;
